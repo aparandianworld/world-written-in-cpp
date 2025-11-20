4208705193619771ff65de33202fcd7f2bc712eb
Aaron Parandian <aparandian@apple.com>
2025-11-20 11:16:26 -0800

    04. type conversion

---
+++ 05/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main() {
+
+    float fnum;
+    int32_t inum;
+    uint32_t uintnum;
+
+    fnum = -7.66f;
+    inum = fnum; // implicit conversion
+    uintnum = inum; // implicit conversion
+
+    std::cout << "fnum: " << fnum << std::endl;
+    std::cout << "inum: " << inum << std::endl;
+    std::cout << "uintnum: " << uintnum << std::endl; // 2 ^ 32 - 7 because unintnum is unsigned int
+    std::cout << "(int32_t)uintnum: " << (int32_t) uintnum << std::endl; // type conversion
+
+    std::cout << std::endl << std::endl;
+    return 0;
+}
