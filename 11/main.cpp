d60a63ffc426227036285799201a32e9f6d81b99
Aaron Parandian <aparandian@apple.com>
2025-11-21 09:11:41 -0800

    11. using objects with pointers

--- 11/main.cpp
+++ 11/main.cpp
@@ -5,27 +5,23 @@
 
 int main() {
 
-    // pointer to objects
-
     TrafficLight *mainLight;
     TrafficLight *sideLight;
 
     mainLight = new TrafficLight("main street", ColorModern::Green, 15);
     sideLight = new TrafficLight("side street", ColorModern::Red, 24);
 
-    mainLight.printStatus();
-    sideLight.printStatus();
+    mainLight -> printStatus();
+    sideLight -> printStatus();
 
     std::cout << "---" << std::endl;
 
-    mainLight.setColor(ColorModern::Yellow);
-    sideLight.setColor(ColorModern::Green);
+    mainLight -> setColor(ColorModern::Yellow);
+    sideLight -> setColor(ColorModern::Green);
 
-    mainLight.printStatus();
-    sideLight.printStatus();
-
-    delete mainLight;
-    delete sideLight;
+    mainLight -> printStatus();
+    // another syntax
+    (*sideLight).printStatus();
 
     std::cout << std::endl << std::endl;
     return 0;
