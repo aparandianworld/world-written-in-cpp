14ba153dfd394c431e75cd814ac459b3d1d79c3f
Aaron Parandian <aparandian@apple.com>
2025-11-22 10:01:30 -0800

    18. open text file for writing

--- 18/main.cpp
+++ 18/main.cpp
@@ -4,20 +4,47 @@
 
 int main() {
 
-    std::string filename = "./input.txt";
+    std::string input_filename = "./input.txt";
     std::string line;
-    std::ifstream fh;
+    std::ifstream fin;
 
-    fh.open(filename);
-    if (fh.fail()) {
-        std::cout << "Failed to open file: " << filename << std::endl;
+    int numLines = 0;
+
+    fin.open(input_filename);
+    if (fin.fail()) {
+        std::cout << "Failed to open file: " << input_filename << std::endl;
         return 1;
     } else {
-        while (!fh.eof()) {
-            std::getline(fh, line);
+        while (!fin.eof()) {
+            std::getline(fin, line);
             std::cout << line << std::endl;
+            numLines++;
         }
-        fh.close();
+        fin.close();
+    }
+
+    std::cout << "---" << std::endl;
+    std::cout << "Number of lines in " << input_filename << ": " << numLines << std::endl;
+
+    std::string output_filename = "./output.txt";
+    std::ofstream fout;
+
+    float a = 3.14f;
+    float b = 5.92f;
+
+    fout.open(output_filename);
+    if (fout.fail()) {
+        std::cout << "Failed to open file: " << output_filename << std::endl;
+        return 1;
+    } else {
+        fout << "a = " << a << ", b = " << b << std::endl;
+        fout << "a + b = " << a + b << std::endl;
+        fout << "a - b = " << a - b << std::endl;
+        fout << "a * b = " << a * b << std::endl;
+        fout << "a / b = " << a / b << std::endl;
+        fout.close();
+
+        std::cout << "Results written to " << output_filename << "successfully." << std::endl;
     }
 
     std::cout << std::endl << std::endl;
